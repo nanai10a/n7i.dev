c52bdf760ed8666c19af7134f5005253ddfb78b8
Nanai Jua <me@n7i.dev>
2022-07-03 12:32:41 +0900

    💥 replace project with node to with deno completely.

--- node/scripts/build/log.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-env node, es2022 */
-
-import pino from "pino";
-import pino_pretty from "pino-pretty";
-
-const stream = pino_pretty();
-const logger = pino(stream);
-
-export default logger;
